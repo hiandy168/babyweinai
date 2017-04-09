0a851f9959e2cb571c4866c2aff36095406ea183
kklldog <kklldog@qq.com>
2017-04-10 01:58:58 +0800

    update

--- pages/history/history.js
+++ pages/history/history.js
@@ -1,21 +1,118 @@
 var historyList = [
-    { id: '1', title: '2017-01-01' },
-    { id: '2', title: '2017-02-01' },
-    { id: '3', title: '2017-01-01' },
-    { id: '4', title: '2017-02-01' },
-    { id: '5', title: '2017-01-01' },
-    { id: '6', title: '2017-02-01' },
-    { id: '7', title: '2017-01-01' },
-    { id: '8', title: '2017-02-01' },
-    { id: '9', title: '2017-01-01' },
-    { id: '10', title: '2017-02-01' },
-    { id: '11', title: '2017-01-01' },
-    { id: '12', title: '2017-02-01' },
-    { id: '13', title: '2017-01-01' },
-    { id: '14', title: '2017-02-01' },
-    { id: '15', title: '2017-01-01' },
-    { id: '16', title: '2017-02-01' }
+    {
+        id: '1', title: '2017-01-01', records: [
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:01', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:02', count: 100, unit: 'ml', type: '开水' }
+        ]
+    },
+    {
+        id: '2', title: '2017-02-01', records: [
+            { datetime: '2017-01-01 17:01', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:02', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:03', count: 100, unit: 'ml', type: '开水' }
+        ]
+    },
+    {
+        id: '3', title: '2017-01-01', records: [
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:01', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:02', count: 100, unit: 'ml', type: '开水' }
+        ]
+    },
+    {
+        id: '4', title: '2017-02-01', records: [
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:01', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:02', count: 100, unit: 'ml', type: '开水' }
+        ]
+    },
+    {
+        id: '5', title: '2017-01-01', records: [
+            { datetime: '2017-01-01 17:01', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:02', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:03', count: 100, unit: 'ml', type: '开水' }
+        ]
+    },
+    {
+        id: '6', title: '2017-02-01', records: [
+            { datetime: '2017-01-01 17:01', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:02', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:03', count: 100, unit: 'ml', type: '开水' }
+        ]
+    },
+    {
+        id: '7', title: '2017-01-01', records: [
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' }
+        ]
+    },
+    {
+        id: '8', title: '2017-02-01', records: [
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' }
+        ]
+    },
+    {
+        id: '9', title: '2017-01-01', records: [
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' }
+        ]
+    },
+    {
+        id: '10', title: '2017-02-01', records: [
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' }
+        ]
+    },
+    {
+        id: '11', title: '2017-01-01', records: [
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' }
+        ]
+    },
+    {
+        id: '12', title: '2017-02-01', records: [
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' }
+        ]
+    },
+    {
+        id: '13', title: '2017-01-01', records: [
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' }
+        ]
+    },
+    {
+        id: '14', title: '2017-02-01', records: [
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' }
+        ]
+    },
+    {
+        id: '15', title: '2017-01-01', records: [
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' }
+        ]
+    },
+    {
+        id: '16', title: '2017-02-01', records: [
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' },
+            { datetime: '2017-01-01 17:00', count: 100, unit: 'ml', type: '开水' }
+        ]
+    }
 ];
+var recordStartX = 0;
 Page(
     {
         data: {
@@ -28,13 +125,36 @@ Page(
                 var item = historyList[i];
                 if (item.id === id) {
                     item.open = !item.open;
-                }else{
+                } else {
                     item.open = false;
                 }
             }
             this.setData({
-                historyList:historyList
+                historyList: historyList
             });
+        }
+        ,
+        recordStart:function(e){
+            recordStartX = e.touches[0].clientX;
+            console.log('startX ',recordStartX);
+        }
+        ,
+        recordMove:function(e){
+            var x = e.touches[0].clientX;
+            var mX = recordStartX - x;
+            console.log('x ' ,x);
+            console.log('mx ',mX);
+            var historyList = this.data.historyList;
+            var item = historyList[0];
+            var records = item.records;
+            records[0].offsetX=20-mX;
+            this.setData({
+                historyList: historyList
+            });
+        }
+        ,
+        recordEnd:function(e){
+
         }
 
     }
